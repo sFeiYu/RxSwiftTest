a8adf5d425926bd0a043de90cfcc29aa9f3b1897
qianfeiying <qianfeiying@yaoyouke.com>
2019-10-22 15:13:50 +0800

    11

---
+++ RXSwiftText/Demo5.swift
@@ -0,0 +1,33 @@
+//
+//  Demo5.swift
+//  RXSwiftText
+//
+//  Created by KnowChat03 on 2019/10/22.
+//  Copyright © 2019 钱飞英. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class Demo5: UIViewController {
+    let disposebag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
